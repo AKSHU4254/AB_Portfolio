10128f30c745f90f994c0fc09f8c3ad6dae1da4a
Akshay Bauskar <akshaybauskar47@gmail.com>
2024-11-27 14:19:56 +0530

    new changes

--- src/Components/PricingCard.js
+++ src/Components/PricingCard.js
@@ -10,7 +10,7 @@ const PricingCard = () => {
         <div className="card">
           <h3>- Basic -</h3>
           <span className="bar"></span>
-          <p className="btc">$ 100</p>
+          <p className="btc">₹ 3500</p>
           <p>- 3 Days -</p>
           <p>- 3 Pages -</p>
           <p>- Featured -</p>
@@ -23,8 +23,8 @@ const PricingCard = () => {
         <div className="card">
           <h3>- Premium -</h3>
           <span className="bar"></span>
-          <p className="btc">$ 200</p>
-          <p>- 2 Days -</p>
+          <p className="btc">₹ 5000</p>
+          <p>- 3 Days -</p>
           <p>- 5 Pages -</p>
           <p>- Featured -</p>
           <p>- Responsive Design -</p>
@@ -36,8 +36,8 @@ const PricingCard = () => {
         <div className="card">
           <h3>- Business -</h3>
           <span className="bar"></span>
-          <p className="btc">$ 300</p>
-          <p>- 5 Days -</p>
+          <p className="btc">₹ 9000</p>
+          <p>- 7 Days -</p>
           <p>- 8 pages -</p>
           <p>- Featured -</p>
           <p>- Responsive Design -</p>
--- src/Components/WorkCardData.js
+++ src/Components/WorkCardData.js
@@ -1,6 +1,8 @@
 import pro1 from "../assets/SummerTravel.png"
 import pro2 from "../assets/Travalworld.png"
 import pro3 from "../assets/BlueE-Commerce.png"
+import pro4 from "../assets/Food Delivey App.jpg"
+
 
 const ProjectCardData = [
     {
@@ -10,6 +12,12 @@ const ProjectCardData = [
         view:"https://github.com/AKSHU4254/Tour_Management"
     },
     {
+        imgsrc: pro4,
+        title: "Food Delivery App",
+        text: "Best Choice for small scale business who want to improve there market",
+        view:"https://github.com/AKSHU4254/Food_Delivery_App"
+    },
+    {
         imgsrc: pro2,
         title: "Travel Project",
         text: "Gives brif idea about how tour management system works",
@@ -17,9 +25,11 @@ const ProjectCardData = [
     },
     {
         imgsrc: pro3,
-        title: "E Commerce",
+        title: "E-Commerce",
         text: "This gives me idea about ecommerce website work and how to deal with cart",
         view:"https://github.com/AKSHU4254/E_Commerce"
     }
+    
+    
 ]
 export default ProjectCardData;
